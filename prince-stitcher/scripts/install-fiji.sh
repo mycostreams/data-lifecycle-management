1a0a6a114ad92ef9a51b8975c8b5a32abb1f2fb5
Isaac Williams <isaac.andrew.williams@gmail.com>
2024-03-14 14:35:02 +0100

    Add actions

--- prince-stitcher/scripts/install-fiji.sh
+++ prince-stitcher/scripts/install-fiji.sh
@@ -9,10 +9,8 @@ then
   echo "Installing Fiji"
   wget https://downloads.imagej.net/fiji/latest/fiji-nojre.zip \
       && unzip fiji-nojre.zip \
-      && rm fiji-nojre.zip
-  echo "Installing Fiji"
+      && rm fiji-nojre.zip \
+      && echo "Installation complete"
 else
   echo "Fiji already installed"
 fi
-
-exit 0
