2627823418d907fc5036606aac30eca60093cfb0
Isaac Williams <isaac.andrew.williams@gmail.com>
2024-04-12 09:39:40 +0200

    dev

--- slurm-uploader/scripts/archive.sh
+++ slurm-uploader/scripts/archive.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 #
 #SBATCH --partion=staging
+#SBATCH --export=USER
 #
 # Archive all images for a given day. 
 # Step 1: Download all images for a given day
 # Step 2: Insert them into a per experiment archive
+#
+# TODO: Delete files when complete.
 
-# TODO: set correct directory based on user
-ARCHIVE_DIR=/archive
-
-while getopts "d:p:" opt; do
+while getopts "d:" opt; do
    case "$opt" in
        d) DATE_STR=${OPTARG};;
-       p) ARCHIVE_DIR=${OPTARG};;
    esac
 done
 
+
 DOWNLOAD_DIR=$(mktemp -d)
+ARCHIVE_DIR="/archive/$USER/"
 TARGET_FILE=$DATE_STR.tar
 
-rclone copy s3:mycostreams "$DOWNLOAD_DIR" --include "*/$DATE_STR*.tar.gz"
+# Download the data
+rclone copy swift:mycostreams "$DOWNLOAD_DIR" --include "*/$DATE_STR*.tar"
 
 if [ "$(ls -A $DOWNLOAD_DIR)" ]; then
     for EXPERIMENT_DIR in ${DOWNLOAD_DIR}/*/; 
