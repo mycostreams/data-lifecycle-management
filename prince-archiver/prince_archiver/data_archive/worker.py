202ce18f33913ff648642167d4e74cf5177d05a9
iw108 <35532554+iw108@users.noreply.github.com>
2024-05-28 14:49:03 +0200

    prince-archiver: update compose
    ## Description
    Update docker compose files and `README.md`. 
    
    
    ## Implementation
    - Use mapping notation for compose files
    - Update `README.md`
    - Don't use `env_file` for prod compose files
    - Use Grafana alloy with Prometheus and Loki integrations
    - Fix cron job timings.

--- prince-archiver/prince_archiver/data_archive/worker.py
+++ prince-archiver/prince_archiver/data_archive/worker.py
@@ -3,6 +3,7 @@ import os
 from contextlib import AsyncExitStack
 from datetime import date, timedelta
 from uuid import UUID, uuid4
+from zoneinfo import ZoneInfo
 
 from arq import cron
 from arq.connections import RedisSettings
@@ -22,13 +23,15 @@ LOGGER = logging.getLogger(__name__)
 
 async def run_archiving(ctx: dict, *, _date: date | None = None):
     job_id = ctx["internal_job_id"]
+    settings: ArchiveWorkerSettings = ctx["settings"]
     archiver: AbstractArchiver | None = ctx["archiver"]
 
-    archive_date = _date or date.today() - timedelta(days=3)
+    delta = timedelta(days=settings.ARCHIVE_TRANSITION_DAYS)
+    archive_files_from = _date or date.today() - delta
 
     if archiver:
-        LOGGER.info("[%s] Initiating archiving for %s", job_id, archive_date)
-        await archiver.archive(archive_date)
+        LOGGER.info("[%s] Initiating archiving for %s", job_id, archive_files_from)
+        await archiver.archive(archive_files_from)
 
 
 async def delete_expired_uploads(ctx: dict, *, _date: date | None = None):
@@ -102,13 +105,15 @@ class WorkerSettings:
     queue_name = "arq:queue-cron"
 
     cron_jobs = [
-        cron(run_archiving, hour={2}, timeout=timedelta(hours=2)),
-        cron(delete_expired_uploads, hour={3}, timeout=timedelta(hours=1)),
+        cron(run_archiving, hour={2}, minute={0}, timeout=timedelta(minutes=2)),
+        cron(delete_expired_uploads, hour={3}, minute={0}, timeout=timedelta(hours=1)),
     ]
 
     on_startup = startup
     on_shutdown = shutdown
     on_job_start = job_start
+
+    timezone = ZoneInfo("Europe/Amsterdam")
 
     redis_settings = RedisSettings.from_dsn(
         os.getenv("REDIS_DSN", "redis://localhost:6379"),
