b79f926f1733eda9d13ac9898eb3b7e60b9f5572
Isaac Williams <isaac.andrew.williams@gmail.com>
2024-05-31 09:19:33 +0200

    add cleanup

--- prince-archiver/scripts/run-integration-tests.sh
+++ prince-archiver/scripts/run-integration-tests.sh
@@ -1,9 +1,18 @@
 #! /usr/bin/env sh
 set -e
 
-docker compose -f tests/compose.integration.yml up --detach --wait
+COMPOSE_FILE=tests/compose.integration.yml
+
+function compose-down {
+    docker compose -f ${COMPOSE_FILE} down --volumes --remove-orphans
+}
+trap compose-down EXIT
+
+
+export POSTGRES_DSN="postgresql+asyncpg://postgres:postgres@localhost:5431/postgres"
+
+compose-down
+docker compose -f ${COMPOSE_FILE} up --detach --wait
 
 poetry run alembic upgrade head
 poetry run pytest tests ${@} -m integration
-
-docker compose -f tests/compose.integration.yml down --volumes --remove-orphans
