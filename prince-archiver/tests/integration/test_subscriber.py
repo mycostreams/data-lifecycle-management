8a921b8e6f731caf52e90b6362510f15b6c8f3a9
Isaac Williams <isaac.andrew.williams@gmail.com>
2024-06-05 10:03:00 +0200

    Add subscriber integration tests

---
+++ prince-archiver/tests/integration/test_subscriber.py
@@ -0,0 +1,63 @@
+import asyncio
+from uuid import uuid4
+
+import pytest
+from aio_pika import Message, connect
+from aio_pika.abc import AbstractExchange, AbstractIncomingMessage
+
+from prince_archiver.data_archive.subscriber import ExchangeConfig, ManagedSubscriber
+
+
+@pytest.fixture(name="exchange_config")
+def fixture_exchange_config() -> ExchangeConfig:
+    return ExchangeConfig(name=uuid4().hex)
+
+
+@pytest.fixture(name="exchange")
+async def fixture_exchange(exchange_config: ExchangeConfig):
+
+    connection = await connect("amqp://guest:guest@rabbitmq:5671")
+
+    async with connection:
+        channel = await connection.channel()
+        exchange = await channel.declare_exchange(**exchange_config.__dict__)
+
+        yield exchange
+
+
+class MessageHandler:
+
+    def __init__(self, event: asyncio.Event | None = None):
+        self.messages = []
+        self.event = asyncio.Event() or event
+
+    async def __call__(self, message: AbstractIncomingMessage):
+        async with message:
+            self.messages.append(message.body)
+            self.event.set()
+
+    async def waiter(self, timeout: int = 10):
+        try:
+            await asyncio.wait_for(self.event.wait(), timeout)
+        except TimeoutError:
+            pass
+
+
+async def test_managed_subscriber(
+    exchange: AbstractExchange,
+    exchange_config: ExchangeConfig,
+):
+    message_handler = MessageHandler()
+
+    subscriber = ManagedSubscriber(
+        "amqp://guest:guest@rabbitmq:5671",
+        message_handler=message_handler,
+        exchange_config=exchange_config,
+    )
+    async with subscriber:
+        message_body = uuid4().hex.encode()
+
+        await exchange.publish(Message(message_body=message_body), routing_key="misc")
+
+        await message_handler.waiter()
+        assert message_body in message_handler.messages
