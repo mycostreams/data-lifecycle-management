5adefa67d0996f2fb413c97b32dcea4ce2a25be2
Isaac Williams <isaac.andrew.williams@gmail.com>
2024-07-08 11:42:25 +0200

    typo

--- prince-stitcher/scripts/run-tests.sh
+++ prince-stitcher/scripts/run-tests.sh
@@ -2,4 +2,4 @@
 set -e
 
 docker compose -f compose.test.yml build --quiet
-docker compose -f compose.test.yml run -ti pytest tests
+docker compose -f compose.test.yml run -ti image-stitcher pytest tests
