d1762df28af3da06eb7a5d959e105b8e19ace233
Isaac Williams <isaac.andrew.williams@gmail.com>
2024-07-09 10:02:54 +0200

    update workflow

--- prince-stitcher/scripts/run-tests.sh
+++ prince-stitcher/scripts/run-tests.sh
@@ -2,4 +2,4 @@
 set -e
 
 docker compose -f compose.test.yml build --quiet
-docker compose -f compose.test.yml run -ti image-stitcher pytest tests
+docker compose -f compose.test.yml run  --rm image-stitcher pytest tests
