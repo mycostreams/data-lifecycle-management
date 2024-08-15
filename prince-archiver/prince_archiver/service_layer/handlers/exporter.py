d3e302294b306594d4acfcd97e420eef39d78fe6
iw108 <35532554+iw108@users.noreply.github.com>
2024-08-15 11:44:18 +0200

    prince-archiver: Add export handlers

---
+++ prince-archiver/prince_archiver/service_layer/handlers/exporter.py
@@ -0,0 +1,132 @@
+"""Handlers used to export imaging event bundles."""
+
+import asyncio
+import logging
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Callable
+from uuid import uuid4
+
+import s3fs
+
+from prince_archiver.adapters.file import ArchiveFileManager, SrcPath
+from prince_archiver.domain.models import EventArchive, ImagingEvent, ObjectStoreEntry
+from prince_archiver.service_layer import messages
+from prince_archiver.service_layer.messagebus import AbstractHandler
+from prince_archiver.service_layer.uow import AbstractUnitOfWork
+
+LOGGER = logging.getLogger(__name__)
+
+
+@dataclass
+class Context:
+    base_path: Path
+    key_generator: Callable[[ImagingEvent], str]
+
+
+async def initiate_imaging_event_export(
+    message: messages.InitiateExportEvent,
+    uow: AbstractUnitOfWork,
+    *,
+    context: Context,
+):
+    """
+    Initiate the export of an image timestep.
+    """
+    async with uow:
+        imaging_event = await uow.imaging_events.get_by_ref_id(message.ref_id)
+        if not imaging_event or imaging_event.event_archive:
+            raise ValueError("No such imaging event")
+
+        uow.add_message(
+            messages.ExportImagingEvent(
+                ref_id=imaging_event.ref_id,
+                type=imaging_event.type,
+                local_path=context.base_path / imaging_event.local_path,
+                target_key=context.key_generator(imaging_event),
+            )
+        )
+
+
+class ExportHandler(AbstractHandler[messages.ExportImagingEvent]):
+    """
+    Class handles export of local images to cloud.
+    """
+
+    def __init__(
+        self,
+        s3: s3fs.S3FileSystem,
+        file_manager: ArchiveFileManager | None = None,
+    ):
+        self.s3 = s3
+        self.file_manager = file_manager or ArchiveFileManager()
+
+    async def __call__(
+        self,
+        message: messages.ExportImagingEvent,
+        uow: AbstractUnitOfWork,
+    ):
+        async with uow:
+            src_dir = SrcPath(message.local_path)
+            async with (
+                self.file_manager.get_temp_archive(src_dir) as archive_path,
+                asyncio.TaskGroup() as tg,
+            ):
+                checksum_task = tg.create_task(
+                    self.file_manager.get_archive_checksum(archive_path),
+                )
+
+                file_count_task = tg.create_task(
+                    self.file_manager.get_file_count(src_dir),
+                )
+
+                size_task = tg.create_task(
+                    self.file_manager.get_archive_size(archive_path),
+                )
+
+                tg.create_task(
+                    self.s3._put_file(archive_path, message.target_key),
+                )
+
+            uow.add_message(
+                messages.ExportedImagingEvent(
+                    ref_id=message.ref_id,
+                    checksum=checksum_task.result(),
+                    img_count=file_count_task.result(),
+                    size=size_task.result(),
+                    key=message.target_key,
+                )
+            )
+            await uow.commit()
+
+
+async def persist_imaging_event_export(
+    message: messages.ExportedImagingEvent,
+    uow: AbstractUnitOfWork,
+):
+    """
+    Persist imaging event export.
+    """
+
+    async with uow:
+        imaging_event = await uow.imaging_events.get_by_ref_id(message.ref_id)
+        if not imaging_event:
+            raise ValueError("No such imaging event")
+
+        imaging_event.add_event_archive(
+            EventArchive(
+                id=uuid4(),
+                size=message.size,
+                img_count=message.img_count,
+                checksum=message.checksum,
+            )
+        )
+
+        imaging_event.add_object_store_entry(
+            ObjectStoreEntry(
+                id=uuid4(),
+                key=message.key,
+                uploaded_at=message.timestamp,
+            )
+        )
+        await uow.commit()
--- prince-archiver/tests/unit/conftest.py
+++ prince-archiver/tests/unit/conftest.py
@@ -1,5 +1,11 @@
+from datetime import UTC, datetime
+from uuid import uuid4
+
 import pytest
 
+from prince_archiver.definitions import System
+from prince_archiver.domain.models import EventArchive, StitchEvent
+from prince_archiver.domain.value_objects import Checksum, Location
 from prince_archiver.models import DataArchiveEntry, ObjectStoreEntry, Timestep
 
 
@@ -16,3 +22,36 @@ def unarchived_timestep() -> Timestep:
     return Timestep(
         object_store_entry=ObjectStoreEntry(key="test/b"),
     )
+
+
+@pytest.fixture(name="unexported_stitch_event")
+def fixture_unexported_stitch_event() -> StitchEvent:
+    return StitchEvent.factory(
+        ref_id=uuid4(),
+        experiment_id="test_experiment_id",
+        local_path="unexported/path",
+        timestamp=datetime(2000, 1, 1, tzinfo=UTC),
+        location=Location(system=System.PRINCE, position=1),
+    )
+
+
+@pytest.fixture(name="exported_stitch_event")
+def fixture_exported_stitch_event() -> StitchEvent:
+    stitch_event = StitchEvent.factory(
+        ref_id=uuid4(),
+        experiment_id="test_experiment_id",
+        local_path="exported/path",
+        timestamp=datetime(2001, 1, 1, tzinfo=UTC),
+        location=Location(system=System.PRINCE, position=1),
+    )
+
+    stitch_event.add_event_archive(
+        EventArchive(
+            id=uuid4(),
+            size=10,
+            img_count=10,
+            checksum=Checksum(hex="321"),
+        )
+    )
+
+    return stitch_event
