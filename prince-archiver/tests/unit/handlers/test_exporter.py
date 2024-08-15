7fcdb53b99e42237e31f390428c1f219e66d3628
Isaac Williams <isaac.andrew.williams@gmail.com>
2024-08-15 11:37:20 +0200

    add export handlers

---
+++ prince-archiver/tests/unit/handlers/test_exporter.py
@@ -0,0 +1,173 @@
+from datetime import UTC, datetime
+from pathlib import Path
+from typing import Awaitable, Callable
+from unittest.mock import AsyncMock
+from uuid import uuid4
+
+import pytest
+from s3fs import S3FileSystem
+
+from prince_archiver.adapters.file import ArchiveFileManager
+from prince_archiver.definitions import EventType
+from prince_archiver.domain.models import StitchEvent
+from prince_archiver.domain.value_objects import Checksum
+from prince_archiver.service_layer.handlers.exporter import (
+    Context,
+    ExportHandler,
+    initiate_imaging_event_export,
+    persist_imaging_event_export,
+)
+from prince_archiver.service_layer.messages import (
+    ExportedImagingEvent,
+    ExportImagingEvent,
+    InitiateExportEvent,
+)
+from prince_archiver.service_layer.uow import AbstractUnitOfWork
+
+from .utils import MockImagingEventRepo, MockUnitOfWork
+
+HandlerT = Callable[[InitiateExportEvent, AbstractUnitOfWork], Awaitable[None]]
+
+
+@pytest.fixture(name="mock_file_manager")
+def fixture_mock_file_manager() -> ArchiveFileManager:
+    mock = AsyncMock(ArchiveFileManager)
+
+    mock.get_archive_checksum.return_value = Checksum(hex="test")
+    mock.get_archive_size.return_value = 1024
+    mock.get_file_count.return_value = 1
+    mock.get_temp_archive.return_value.__aenter__ = AsyncMock(return_value=Path("test"))
+
+    return mock
+
+
+@pytest.fixture(name="uow")
+def fixture_uow(
+    exported_stitch_event: StitchEvent,
+    unexported_stitch_event: StitchEvent,
+):
+    return MockUnitOfWork(
+        imaging_event_repo=MockImagingEventRepo(
+            imaging_events=[exported_stitch_event, unexported_stitch_event],
+        ),
+    )
+
+
+@pytest.fixture(name="context_bound_handler")
+def fixture_context_bound_handler() -> HandlerT:
+    context = Context(
+        base_path=Path("/test/"),
+        key_generator=lambda _: "test.tar",
+    )
+
+    def _handler(msg: InitiateExportEvent, uow: AbstractUnitOfWork):
+        return initiate_imaging_event_export(msg, uow, context=context)
+
+    return _handler
+
+
+async def test_export_handler_successful(
+    mock_file_manager: ArchiveFileManager, uow: MockUnitOfWork
+):
+    handler = ExportHandler(AsyncMock(S3FileSystem), mock_file_manager)
+
+    msg = ExportImagingEvent(
+        ref_id=uuid4(),
+        type=EventType.STITCH,
+        local_path=Path("test"),
+        target_key="target/key",
+    )
+
+    await handler(msg, uow)
+
+    msg = next(uow.collect_messages())
+
+    assert isinstance(msg, ExportedImagingEvent)
+    assert msg.img_count == 1
+    assert msg.checksum == Checksum(hex="test")
+    assert msg.size == 1024
+    assert msg.key == "target/key"
+
+    assert uow.is_commited
+
+
+async def test_initiate_imaging_event_export_successful(
+    context_bound_handler: HandlerT,
+    uow: MockUnitOfWork,
+    unexported_stitch_event: StitchEvent,
+):
+    msg = InitiateExportEvent(
+        ref_id=unexported_stitch_event.ref_id, type=EventType.STITCH
+    )
+
+    await context_bound_handler(msg, uow)
+
+    expected_msg = ExportImagingEvent(
+        ref_id=unexported_stitch_event.ref_id,
+        type=EventType.STITCH,
+        local_path=Path("/test/unexported/path"),
+        target_key="test.tar",
+    )
+
+    assert next(uow.collect_messages()) == expected_msg
+
+
+async def test_initiate_imaging_event_export_non_existent_reference(
+    context_bound_handler: HandlerT,
+):
+    msg = InitiateExportEvent(
+        ref_id=uuid4(),
+        type=EventType.STITCH,
+    )
+
+    with pytest.raises(ValueError):
+        await context_bound_handler(msg, MockUnitOfWork())
+
+
+async def test_initiate_imaging_event_already_exported(
+    context_bound_handler: HandlerT,
+    uow: MockUnitOfWork,
+    exported_stitch_event: StitchEvent,
+):
+    msg = InitiateExportEvent(
+        ref_id=exported_stitch_event.ref_id,
+        type=EventType.STITCH,
+    )
+
+    with pytest.raises(ValueError):
+        await context_bound_handler(msg, uow)
+
+
+async def test_persist_imaging_event_successful(
+    uow: MockUnitOfWork,
+    unexported_stitch_event: StitchEvent,
+):
+    msg = ExportedImagingEvent(
+        ref_id=unexported_stitch_event.ref_id,
+        checksum=Checksum("test"),
+        img_count=5,
+        size=1024,
+        key="target",
+        timestamp=datetime(2000, 1, 1, tzinfo=UTC),
+    )
+
+    await persist_imaging_event_export(msg, uow)
+
+    assert unexported_stitch_event.event_archive
+    assert unexported_stitch_event.object_store_entry
+
+    assert uow.is_commited
+
+
+async def test_persist_imaging_event_non_existent_reference():
+    msg = ExportedImagingEvent(
+        ref_id=uuid4(),
+        checksum=Checksum("test"),
+        img_count=5,
+        size=1024,
+        key="target",
+        timestamp=datetime(2000, 1, 1, tzinfo=UTC),
+    )
+
+    with pytest.raises(ValueError):
+        await persist_imaging_event_export(msg, MockUnitOfWork())
---
+++ prince-archiver/tests/unit/handlers/utils.py
@@ -0,0 +1,59 @@
+from datetime import date
+from typing import Generator, Mapping
+from uuid import UUID
+
+from pydantic import BaseModel
+
+from prince_archiver.adapters.repository import AbstractImagingEventRepo
+from prince_archiver.domain.models import ImagingEvent, StitchEvent, VideoEvent
+from prince_archiver.service_layer.uow import AbstractUnitOfWork
+
+
+class MockImagingEventRepo(AbstractImagingEventRepo):
+    def __init__(
+        self,
+        imaging_events: list[ImagingEvent] | None = None,
+    ):
+        self.imaging_events: Mapping[UUID, ImagingEvent] = {
+            item.ref_id: item for item in imaging_events or []
+        }
+
+    def add(self, image_event: StitchEvent | VideoEvent) -> None:
+        self.imaging_events[image_event.ref_id] = image_event
+
+    async def get_by_ref_id(self, event_id: UUID) -> ImagingEvent | None:
+        return self.imaging_events.get(event_id)
+
+    async def get_by_ref_date(self, date: date) -> list[ImagingEvent]:
+        filtered_results = filter(
+            lambda item: item.timestamp.date() == date, self.imaging_events.values()
+        )
+        return list(filtered_results)
+
+
+class MockUnitOfWork(AbstractUnitOfWork):
+    def __init__(
+        self,
+        imaging_event_repo: AbstractImagingEventRepo | None = None,
+    ):
+        self.imaging_events = imaging_event_repo or MockImagingEventRepo()
+        self.messages = []
+        self.is_commited = False
+
+    def add_message(self, message: BaseModel):
+        self.messages.append(message)
+
+    async def __aenter__(self) -> AbstractUnitOfWork:
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, exc_traceback):
+        return await super().__aexit__(exc_type, exc_value, exc_traceback)
+
+    async def commit(self) -> None:
+        self.is_commited = True
+
+    async def rollback(self) -> None:
+        return await super().rollback()
+
+    def collect_messages(self) -> Generator[BaseModel, None, None]:
+        yield from self.messages
