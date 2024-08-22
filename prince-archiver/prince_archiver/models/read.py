44bf2d734adb933753dfad793bf9d99c5f23a03e
Isaac Williams <isaac.andrew.williams@gmail.com>
2024-08-22 16:09:55 +0200

    Also return type

--- prince-archiver/prince_archiver/models/read.py
+++ prince-archiver/prince_archiver/models/read.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from sqlalchemy import select
 from sqlalchemy.orm import DeclarativeBase, Mapped
 
+from prince_archiver.definitions import EventType
 from .v2 import ImagingEvent, ObjectStoreEntry
 
 
@@ -16,6 +17,7 @@ class Export(ReadBase):
         select(
             ImagingEvent.id,
             ImagingEvent.ref_id,
+            ImagingEvent.type,
             ImagingEvent.experiment_id,
             ObjectStoreEntry.key,
             ObjectStoreEntry.uploaded_at,
@@ -27,5 +29,6 @@ class Export(ReadBase):
     id: Mapped[UUID]
     ref_id: Mapped[UUID]
     experiment_id: Mapped[str]
+    type: Mapped[EventType]
     key: Mapped[str]
     uploaded_at: Mapped[datetime]
