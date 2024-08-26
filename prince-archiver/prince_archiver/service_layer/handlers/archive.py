a9d0cf5c010a690f6e9afbb71cef2dd7fc9c3c92
Isaac Williams <isaac.andrew.williams@gmail.com>
2024-08-26 16:11:02 +0200

    make logging clearer

--- prince-archiver/prince_archiver/service_layer/handlers/archive.py
+++ prince-archiver/prince_archiver/service_layer/handlers/archive.py
@@ -15,7 +15,7 @@ async def add_data_archive_entry(
     message: AddDataArchiveEntry,
     uow: AbstractUnitOfWork,
 ):
-    LOGGER.info("[%s] Updating archive entries", message.job_id)
+    LOGGER.info("[%s] Updating archive entries %s", message.job_id, message.path)
 
     async with uow:
         if await uow.data_archive.get_by_path(message.path):
