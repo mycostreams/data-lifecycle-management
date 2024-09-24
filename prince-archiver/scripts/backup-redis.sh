f3cfa5471ccd5362f7a4b65d4bd2e2cb5f559a55
Isaac Williams <isaac.andrew.williams@gmail.com>
2024-09-24 13:49:51 +0200

    make script executable

--- prince-archiver/scripts/backup-redis.sh
+++ prince-archiver/scripts/backup-redis.sh
@@ -4,5 +4,5 @@ cd "$(dirname $(dirname $0))"
 
 FILENAME="$(date +%s).bak"
 
-docker compose run -f compose.yml -f compose.prod --rm \
+docker compose -f compose.yml -f compose.prod run --rm \
     aws-cli s3 cp redis/dump.rds s3://backups/redis/$FILENAME
