a986d1927d96a779393db1561b00f50d77e98fc0
iw108 <35532554+iw108@users.noreply.github.com>
2024-09-26 15:12:12 +0200

    DA-46 prince-archiver: Use local stack (#80)
    ## Description
    Move from adbove `s3Mock` to `localstack`
    
    ## Implementation
    - Update `compose.dev.yml` file
    - Add init script

---
+++ prince-archiver/scripts/init-dev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cd "$(dirname $(dirname $0))"
+
+docker compose -f compose.yml -f compose.dev.yml run --rm db-migrations
+docker compose -f compose.yml -f compose.dev.yml run --rm prince \
+    prince-cli populate-data-dir
