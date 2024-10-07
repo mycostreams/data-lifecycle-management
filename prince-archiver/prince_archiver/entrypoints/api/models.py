7d38833aaf6cb4706e5d6dd5dfa621d0bda8e92e
Isaac Williams <isaac.andrew.williams@gmail.com>
2024-10-07 14:15:17 +0200

    Add filtering for event_type

--- prince-archiver/prince_archiver/entrypoints/api/models.py
+++ prince-archiver/prince_archiver/entrypoints/api/models.py
@@ -1,9 +1,24 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from uuid import UUID
+from typing import Self
 
-from pydantic import BaseModel, HttpUrl
+from pydantic import AwareDatetime, BaseModel, HttpUrl, model_validator, Field
+
 
 from prince_archiver.definitions import EventType
+from prince_archiver.utils import now
+
+
+class FilterParams(BaseModel):
+    start: AwareDatetime = Field(default_factory=lambda: now() - timedelta(hours=24))
+    end: AwareDatetime = Field(default_factory=now)
+    event_type: EventType = EventType.STITCH
+
+    @model_validator(mode="after")
+    def validate_time_range(self) -> Self:
+        if (self.end - self.start) > timedelta(hours=36):
+            raise ValueError("")
+        return self
 
 
 class ExportModel(BaseModel):
--- prince-archiver/tests/integration/test_read_repo.py
+++ prince-archiver/tests/integration/test_read_repo.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import pytest
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from prince_archiver.definitions import EventType
 from prince_archiver.adapters.repository import ReadRepo
 
 pytestmark = pytest.mark.integration
@@ -13,13 +14,37 @@ def repo(session: AsyncSession) -> ReadRepo:
     return ReadRepo(session)
 
 
+@pytest.mark.parametrize(
+    "start,expected_count",
+    ((datetime(1900, 1, 1), 1), (datetime(2010, 1, 1), 0)),
+)
 @pytest.mark.usefixtures("seed_data")
-async def test_export_within_time_range(repo: ReadRepo):
-    result = await repo.get_exports(start=datetime(1900, 1, 1))
-    assert len(result) == 1
+async def test_export_filters_by_time_range(
+    repo: ReadRepo, 
+    start: datetime, 
+    expected_count: int,
+):
+    result = await repo.get_exports(
+        start=start,
+        end=datetime(2100, 1, 1),
+        event_type=EventType.STITCH
+    )
+    assert len(result) == expected_count
 
 
+@pytest.mark.parametrize(
+    "event_type,expected_count",
+    ((EventType.STITCH, 1), (EventType.VIDEO, 0)),
+)
 @pytest.mark.usefixtures("seed_data")
-async def test_export_outside_time_range(repo: ReadRepo):
-    result = await repo.get_exports(start=datetime(2010, 1, 1))
-    assert len(result) == 0
+async def test_export_filters_on_event_type(
+    repo: ReadRepo, 
+    event_type: EventType, 
+    expected_count:int
+):
+    result = await repo.get_exports(
+        start=datetime(1900, 1, 1),
+        end=datetime(2100, 1, 1),
+        event_type=event_type,
+    )
+    assert len(result) == expected_count
