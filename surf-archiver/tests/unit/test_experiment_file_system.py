febb45e0dd609fc1ec62da723f9131102223da12
iw108 <35532554+iw108@users.noreply.github.com>
2024-10-08 09:50:01 +0200

    DA-54 surf-archiver: Update path regex (#88)
    ## Description
    Latest changes in `prince-archiver` resulted in changes to the object storage key structure. These need to be reflected in `surf-archiver`
    
    ## Implementation
    - `f"{self.bucket_name}/{mode}/*/{date_prefix}*.tar"` -> `f"{self.bucket_name}/{mode}/*/{date_prefix}/*.tar"`
    - bump version

--- surf-archiver/tests/unit/test_experiment_file_system.py
+++ surf-archiver/tests/unit/test_experiment_file_system.py
@@ -9,18 +9,18 @@ from surf_archiver.file import ExperimentFileSystem
 async def test_list_files_by_date():
     mock_s3 = AsyncMock(S3FileSystem)
     mock_s3._glob.return_value = [
-        "test-bucket/images/id-1/20000101_0000.tar",
-        "test-bucket/images/id-2/20000101_0000.tar",
-        "test-bucket/images/id-2/20000101_0100.tar",
+        "test-bucket/images/id-1/20000101/0000.tar",
+        "test-bucket/images/id-2/20000101/0000.tar",
+        "test-bucket/images/id-2/20000101/0100.tar",
     ]
 
     file_system = ExperimentFileSystem(s3=mock_s3, bucket_name="test-bucket")
 
     expected = {
-        "id-1": ["test-bucket/images/id-1/20000101_0000.tar"],
+        "id-1": ["test-bucket/images/id-1/20000101/0000.tar"],
         "id-2": [
-            "test-bucket/images/id-2/20000101_0000.tar",
-            "test-bucket/images/id-2/20000101_0100.tar",
+            "test-bucket/images/id-2/20000101/0000.tar",
+            "test-bucket/images/id-2/20000101/0100.tar",
         ],
     }
 
