b7d617114206d8df944987fb88e66c9706256370
iw108 <35532554+iw108@users.noreply.github.com>
2024-10-14 14:27:36 +0200

    DA-59 surf-archiver: Add docker compose implementation (#93)
    ## Description
    Add docker compose to help facilitate local development
    
    
    ## Implementation
    - Add `remote` sub module
    - Add `compose.yml` and `compose.prod.yml`
    - Update `README.md`

---
+++ surf-archiver/scripts/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+
+if getent passwd "$USER" > /dev/null; then
+    echo "User \"$USER\" already exists"
+else
+    echo "Creating user \"$USER\""
+    useradd -m -s /bin/bash guest
+    echo "$USER:$USER" | chpasswd
+fi
+
+echo "Creating environment file for \"$USER\""
+python /app/surf_archiver/scripts/write_env_file.py 
+
+
+# Allow running other programs, e.g. bash
+if [[ -z "$1" ]]; then
+    echo "Executing sshd"
+    exec /usr/sbin/sshd -D -e
+else
+    echo "Executing $*"
+    /usr/sbin/sshd -q
+    exec "$@"
+fi
---
+++ surf-archiver/scripts/prestart.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+set -e
+set -x
+
+python /app/surf_archiver/scripts/init_data.py
