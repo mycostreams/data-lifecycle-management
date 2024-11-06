45877d051f113f6d60679a9c823bda76475ebeb5
Isaac Williams <isaac.andrew.williams@gmail.com>
2024-11-06 10:05:28 +0100

    move img directory

--- prince-archiver/prince_archiver/test_utils/utils.py
+++ prince-archiver/prince_archiver/test_utils/utils.py
@@ -1,11 +1,6 @@
-from functools import lru_cache
 from pathlib import Path
 from shutil import copy
 from timeit import default_timer
-
-import httpx
-
-DOWNLOAD_URL = "https://vu.data.surfsara.nl/index.php/s/ndI1UoMRwliVYGR/download"
 
 
 class Timer:
@@ -17,21 +12,9 @@ class Timer:
         return default_timer() - self.ref
 
 
-@lru_cache
-def _get_image(url: str = DOWNLOAD_URL):
-    response = httpx.get(url)
-    return response.content
-
-
-def make_timestep_directory(
-    target_dir: Path,
-    src_img: Path | None = None,
-) -> None:
+def make_timestep_directory(target_dir: Path, src_img: Path) -> None:
     """Construct a new timestep directory."""
     target_dir.mkdir(parents=True, exist_ok=True)
 
     img = target_dir / "Img_r10_c15.tif"
-    if src_img:
-        copy(src_img, img)
-    else:
-        img.write_bytes(_get_image())
+    copy(src_img, img)
