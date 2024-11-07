621f1db6dcd52c90b000476c9798ac6375ee166f
Isaac Williams <isaac.andrew.williams@gmail.com>
2024-11-07 09:22:13 +0100

    update networking

--- infrastructure/redis/scripts/backup.sh
+++ infrastructure/redis/scripts/backup.sh
@@ -4,5 +4,5 @@ cd "$(dirname $(dirname $0))"
 
 FILENAME="$(date +%s).bak"
 
-docker compose -f compose.yml -f compose.prod run --rm \
+docker compose run --rm \
     aws-cli s3 cp redis/dump.rds s3://backups/redis/$FILENAME
