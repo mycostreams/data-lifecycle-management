cb328148fbe5a724b3c614f3cf74a7908263d636
Isaac Williams <isaac.andrew.williams@gmail.com>
2024-11-12 16:11:12 +0100

    clean up

--- prince-archiver/prince_archiver/domain/value_objects.py
+++ prince-archiver/prince_archiver/domain/value_objects.py
@@ -2,28 +2,10 @@
 
 from dataclasses import dataclass
 
-from prince_archiver.definitions import Algorithm, System
+from prince_archiver.definitions import Algorithm
 
 
 @dataclass
 class Checksum:
     hex: str
     algorithm: Algorithm = Algorithm.SHA256
-
-
-@dataclass
-class Location:
-    system: System
-    position: int
-
-
-@dataclass
-class FrameSize:
-    row: int
-    col: int
-
-
-@dataclass
-class GridSize:
-    row: int
-    col: int
