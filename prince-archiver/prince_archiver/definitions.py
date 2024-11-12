be4b982790077e367ca14acf8d7b1d6c5d92601c
Isaac Williams <isaac.andrew.williams@gmail.com>
2024-11-12 16:12:32 +0100

    correct enum

--- prince-archiver/prince_archiver/definitions.py
+++ prince-archiver/prince_archiver/definitions.py
@@ -4,7 +4,7 @@ from enum import StrEnum, auto
 class System(StrEnum):
     PRINCE = auto()
     TSU_EXP001 = "tsu-exp001"
-    TSU_EXP001 = "tsu-exp002"
+    TSU_EXP002 = "tsu-exp002"
 
 
 class EventType(StrEnum):
