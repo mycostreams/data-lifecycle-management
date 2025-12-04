4f01deb6967604559af943a6d3c3689f6661fbe2
Simon <simonvanstaalduine@home.nl>
2025-12-04 15:30:25 +0100

    added overview as event type

--- prince-archiver/prince_archiver/service_layer/handlers/utils.py
+++ prince-archiver/prince_archiver/service_layer/handlers/utils.py
@@ -5,7 +5,28 @@ from prince_archiver.service_layer.dto import CommonImagingEvent
 
 
 def get_target_key(imaging_event: CommonImagingEvent, bucket: str) -> str:
-    event_type = "images" if imaging_event.type == EventType.STITCH else "videos"
+    """
+    Creates a target key for an imaging event based on the contents of the message.
+
+    Final path is:
+    /<bucket>/<[videos, images]>/<experiment_id>/<YYYYMMDD>/<HHmmss>.tar
+
+    Args:
+        imaging_event (CommonImagingEvent): Metadata of imaging event
+        bucket (str): Bucket where the video comes goes to
+
+    Returns:
+        str: address where the data will get uploaded
+    """
+    match imaging_event.type:
+        case EventType.STITCH:
+            event_type="images"
+        case EventType.VIDEO:
+            event_type="videos"
+        case EventType.OVERVIEW:
+            event_type="images"
+        case _:
+            event_type="videos"
 
     ref_timestamp = imaging_event.timestamp.astimezone(UTC)
 
