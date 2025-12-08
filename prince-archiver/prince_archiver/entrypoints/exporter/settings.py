7ecbf9d6ba1cdf42b49161ce0185b04fc1d27293
Staalduine <simonvanstaalduine@home.nl>
2025-12-08 19:21:46 +0100

    added documentation

--- prince-archiver/prince_archiver/entrypoints/exporter/settings.py
+++ prince-archiver/prince_archiver/entrypoints/exporter/settings.py
@@ -6,7 +6,7 @@ from prince_archiver.config import AWSSettings
 
 
 class Settings(AWSSettings, BaseSettings):
-    SRC_DIR: Path
+    SRC_DIR: Path  # from compose.yml: /data
     STAGING_DIR: Path | None = None
 
     model_config = SettingsConfigDict(
